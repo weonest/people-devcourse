4c9e99441835d160f7658e917f7afec5e0f7ee1c
weonest <geonhee33@naver.com>
2023-07-18 21:23:00 +0900

    refactor: react 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,69 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import React, {useEffect, useState} from 'react';
+import {ProductList} from "./components/ProductList";
+import {Summary} from "./components/Summary";
+import axios from "axios";
 
 function App() {
+  const [products, setProducts] = useState([
+  ]);
+  const [items, setItems] = useState([]);
+  const handleAddClicked = productId => {
+    const product = products.find(v => v.productId === productId);
+    const found = items.find(v => v.productId === productId);
+    const updatedItems =
+        found ? items.map(v => (v.productId === productId) ? {...v, count: v.count + 1} : v) : [...items, {
+          ...product,
+          count: 1
+        }]
+    setItems(updatedItems);
+  }
+
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/products')
+        .then(v => setProducts(v.data));
+  }, []);
+
+  const handleOrderSubmit = (order) => {
+    if (items.length === 0) {
+      alert("아이템을 추가해 주세요!");
+    } else {
+      axios.post('http://localhost:8080/api/orders', {
+        email: order.email,
+        address: order.address,
+        postcode: order.postcode,
+        orderItems: items.map(v => ({
+          productId: v.productId,
+          category: v.category,
+          price: v.price,
+          quantity: v.count
+        }))
+      }).then(
+          v => alert("주문이 정상적으로 접수되었습니다."),
+          e => {
+            alert("서버 장애");
+            console.error(e);
+          })
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit22 <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="container-fluid">
+        <div className="row justify-content-center m-4">
+          <h1 className="text-center">데브코스의 민족</h1>
+        </div>
+        <div className="card">
+          <div className="row">
+            <div className="col-md-8 mt-4 d-flex flex-column align-items-start p-3 pt-0">
+              <ProductList products={products} onAddClick={handleAddClicked}/>
+            </div>
+            <div className="col-md-4 summary p-4">
+              <Summary items={items} onOrderSubmit={handleOrderSubmit}/>
+            </div>
+          </div>
+        </div>
+      </div>
   );
 }
 
